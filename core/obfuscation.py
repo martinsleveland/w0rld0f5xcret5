9c3b202b5705b33e1cc394c7a3d77f666f8aa336
ℳ𝒶𝓇𝓉𝒾𝓃 𝒮𝓁𝑒𝓋𝑒𝓁𝒶𝓃𝒹 <146337938+martinsleveland@users.noreply.github.com>
2025-05-14 16:05:17 +0200

    Create obfuscation.py

---
+++ core/obfuscation.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+file_input = input("Enter the file path")
+
+isValid = False
+
+def file_input():
+    if != file_input:
+        print("No filepath specified!")
+        isValid = True
+        return
+    else:
+        print("Failed!")
+        isValid = False
+        
+def check_file(file_input):
+    pass
+    
+e

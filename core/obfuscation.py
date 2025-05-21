bff390675dafaee36650aca646d12b2b1b73e9bd
ℳ𝒶𝓇𝓉𝒾𝓃 𝒮𝓁𝑒𝓋𝑒𝓁𝒶𝓃𝒹 <146337938+martinsleveland@users.noreply.github.com>
2025-05-21 16:34:20 +0200

    Update obfuscation.py

--- core/obfuscation.py
+++ core/obfuscation.py
@@ -1,21 +1,30 @@
+import subproccess
 import sys
 import os
-
-file_input = input("Enter the file path")
-
-isValid = False
-
-def file_input():
-    if != file_input:
-        print("No filepath specified!")
-        isValid = True
-        return
-    else:
-        print("Failed!")
-        isValid = False
-        
-def check_file(file_input):
-    pass
     
-def obfuscate_payload(self):
-    print("logic here")
+def obfuscate_payload():
+    file_input = input("Enter the file path")
+
+    obfuscation_type = input("Enter the obfuscation method you wamt to apply: ")
+    if obfuscation_type == None:
+        print("wrong answer")
+        pass
+    elif obfuscation_type == "jigsaw":
+        print("you chose the jigsaw obfuscation method")
+    
+    elif obfuscation_type == ""
+
+
+    isValid = False
+    
+    def file_input():
+        if != file_input:
+            print("No filepath specified!")
+            isValid = True
+            return
+        else:
+            print("Failed!")
+            isValid = False
+            
+    def check_file(file_input):
+        pass

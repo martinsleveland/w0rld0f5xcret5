312bf144cc22f4f38d132a22bc64624ff36d0d1f
ℳ𝒶𝓇𝓉𝒾𝓃 𝒮𝓁𝑒𝓋𝑒𝓁𝒶𝓃𝒹 <146337938+martinsleveland@users.noreply.github.com>
2025-06-05 16:08:44 +0200

    Update evil_twin.py

--- core/evil_twin.py
+++ core/evil_twin.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def run_bash_script(self):
         try:
             # Run the bash script (make sure it's executable)
@@ -6,3 +9,13 @@ def run_bash_script(self):
             self.output_box.setText(output)
         except Exception as e:
             self.output_box.setText(f"💥 Error: {e}")
+        
+def run_deauth(self, ssid, channel, workers):
+        
+        target_ssid = input("ssid: ")
+        target_channel = input("Channel: ")
+        amount_workers = input("Amount of workers: ")
+        
+        self.ssid = target_ssid
+        self.channel = target_channel
+        self.workers = amount_workers

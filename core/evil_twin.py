926c22c0964b7e32e8d66a81e86d41797e39018e
ℳ𝒶𝓇𝓉𝒾𝓃 𝒮𝓁𝑒𝓋𝑒𝓁𝒶𝓃𝒹 <146337938+martinsleveland@users.noreply.github.com>
2025-06-06 16:11:57 +0200

    Update evil_twin.py

--- core/evil_twin.py
+++ core/evil_twin.py
@@ -1,5 +1,8 @@
 import requests
-import scapy
+from scapy.all import *
+
+print("If you want to put network card in monitor mode,")
+print("then you need to type airmon-ng wlan mon or somthing i dont remember")
 
 
 def run_bash_script(self):
@@ -11,12 +14,16 @@ def run_bash_script(self):
         except Exception as e:
             self.output_box.setText(f"💥 Error: {e}")
         
-def run_deauth(self, ssid, channel, workers):
-        
+def run_deauth(self, ssid, channel, interface, workers):
         target_ssid = input("ssid: ")
         target_channel = input("Channel: ")
+        interface = input("Choose WiFi adapter(wlan0mon): ")
         amount_workers = input("Amount of workers: ")
+        
         
         self.ssid = target_ssid
         self.channel = target_channel
         self.workers = amount_workers
+        
+        
+        
